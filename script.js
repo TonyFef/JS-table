818734098b3912dad9fd14231e638fd465013bd0
TonyFef <92264768+TonyFef@users.noreply.github.com>
2022-03-04 19:19:58 +0300

    fixed toggling classes

--- script.js
+++ script.js
@@ -3,22 +3,44 @@ let counter = 0;
 
 const data = fetch("data.json");
 data.then((response) => response.json()).then((data) => {
-    startButton.addEventListener("click", () => {
-        tableShower();
-    });
+    startButton.addEventListener(
+        "click",
+        () => {
+            tableShower();
+        },
+        { once: true }
+    );
 
     tableShower = () => {
         data.forEach((elem) => {
             counter++;
             element = document.createElement("tr");
-            element.parentId = elem.parentId;
+
             element.classList.add(`item-${elem.parentId}`);
+            element.id = `a${counter}`;
 
             element.innerHTML = `<td>${elem.id}</td><td id=a${counter}>${elem.name}</td><td>${elem.email}</td><td class='element'>${elem.balance}</td>`;
             tbody.append(element);
 
+            let selectedTr = element.closest("tr");
 
-            
+            if (selectedTr.classList.value != "item-0") {
+                let parentId = selectedTr.classList.value.substr(5);
+
+                let parent = document.querySelector(`#a${parentId}`).closest("tr");
+
+                parentName = parent.querySelectorAll("td")[1].textContent;
+                parentEmail = parent.querySelectorAll("td")[2].textContent;
+                parentBalance = parent.querySelectorAll("td")[3].textContent;
+
+                let parentShower = document.createElement("tr");
+                parentShower.id = `b${elem.id}`;
+                parentShower.classList.add("pink");
+                parentShower.classList.add("hide");
+                parentShower.innerHTML = `<td></td><td>${parentName}</td><td>${parentEmail}</td><td class='element'>${parentBalance}</td>`;
+
+                selectedTr.after(parentShower);
+            }
         });
 
         onShowParent();
@@ -26,29 +48,14 @@ data.then((response) => response.json()).then((data) => {
 
     onShowParent = () => {
         tbody.addEventListener("click", (e) => {
-            let selectedTr = e.target.closest("tr");
-            console.log(selectedTr);
+            let selectedTr2 = e.target.closest("tr");
+            let smth = selectedTr2.querySelectorAll("td")[0].textContent;
 
-            // if (selectedTr.classList.value.includes("show")) {
-            //     return;
-            // }
-
-            if (selectedTr.classList.value != "item-0") {
-                let parentId = selectedTr.classList.value.substr(5);
-
-                let parent = document.querySelector(`#a${parentId}`).closest("tr");
-                console.log(parent);
-
-                parentName = parent.querySelectorAll("td")[1].textContent;
-                parentEmail = parent.querySelectorAll("td")[2].textContent;
-                parentBalance = parent.querySelectorAll("td")[3].textContent;
-
-                let parentShower = document.createElement("tr");
-                parentShower.classList.add("pink");
-                // parentShower.classList.toggle("hide");
-                parentShower.innerHTML = `<td>${parent.id}</td><td>${parentName}</td><td>${parentEmail}</td><td class='element'>${parentBalance}</td>`;
-                console.log(parentShower);
-                selectedTr.after(parentShower);
+            let temp = document.getElementById(`b${smth}`);
+            if (temp == null) {
+                return;
+            } else {
+                temp.classList.toggle("hide");
             }
         });
     };
